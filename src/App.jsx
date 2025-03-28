81588a29b6d83d1ff0d288414a2373f9b703dd38
erlopez11 <lopezelainy@gmail.com>
2025-03-28 16:34:36 -0500

    wip: import & route mailboxes, mailbox details, & mailbox form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router';
+import MailboxList from './components/MailBoxList/MailboxList';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 import './App.css'
+import { Route } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World!</h1>
+
+      <Routes>
+        <Route
+          path='/'
+          element={<main><h1>Post Office</h1></main>} 
+        />
+
+        <Route 
+          path='/mailboxes'
+          element={<MailboxList />}
+        />
+
+        <Route 
+          path='/new-mailbox'
+          element={<MailboxForm />}
+        />
+
+        <Route 
+          path= '/mailboxes/:mailboxId'
+          element={<MailboxDetails />}
+        />
+      </Routes>
     </>
   )
 }
