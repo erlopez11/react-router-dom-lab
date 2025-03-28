f5a5304e1862b5e46342037456c6857de6df2519
erlopez11 <lopezelainy@gmail.com>
2025-03-28 17:09:29 -0500

    wip:import useState & use Navigate & create new mail box form

--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -1,5 +1,53 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
-const MailboxForm = () => {
+const initialState = {
+    boxOwner: '',
+    boxSize: '',
+
+}
+
+const MailboxForm = (props) => {
+    const [formData, setFormData] = useState(initialState);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.addBox(formData);
+        setFormData(initialState);
+        navigate('/mailboxes');
+    };
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value});
+    };
+
+    return (
+        <main>
+            <h1>New Mailbox</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='boxOwner'>Enter a Boxholder:</label>
+                <input
+                    type='text'
+                    id='boxOwner'
+                    name='boxOwner'
+                    value={formData.boxOwner}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor='boxSize'>Select a Box Size</label>
+                <select id='boxSize' name='boxSize' value={formData.boxSize} onChange={handleChange}>
+                    <option>Select a Size</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='large'>Large</option>
+                </select>
+                <button type='submit'>Submit</button>
+            </form>
+        </main>
+    );
 
 };
 
