3a6a737f53719e708090fc8fc2146fd7b3f7a3e4
erlopez11 <lopezelainy@gmail.com>
2025-03-29 11:10:32 -0500

    wip:add mailboxes as prop for mailbox list

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
         <Route 
           path='/mailboxes'
-          element={<MailboxList />}
+          element={<MailboxList mailboxes={mailboxes}/>}
         />
 
         <Route 
