1439533b4d6f652f5a051cfd13d64f2156542353
erlopez11 <lopezelainy@gmail.com>
2025-03-29 11:11:47 -0500

    wip:import Link & MailboxList returns mailboxlist with Link to ind mailbox

--- src/components/MailBoxList/MailboxList.jsx
+++ src/components/MailBoxList/MailboxList.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router';
 
-const MailboxList = () => {
+const MailboxList = (props) => {
+
+    return (
+        <main>
+            <h1>MailBox List</h1>
+
+            <ul>
+                {props.mailboxes.map((mailbox) => (
+                    <li key={mailbox._id}>
+                        <Link to={`/mailboxes/${mailbox._id}`}>Mailbox {mailbox._id}</Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 
 };
 
